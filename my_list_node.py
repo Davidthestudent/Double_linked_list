52e5efc6edfd346aaf0a454a8850cbf8d5d5b2fc
Davidthestudent <103884270+Davidthestudent@users.noreply.github.com>
2024-05-03 03:49:59 +0300

    Update my_list_node.py

--- my_list_node.py
+++ my_list_node.py
@@ -1,4 +1,4 @@
-class MyListNode:
+class MyListNode:  # Initializing the node of a double linked list
     def __init__(self, elem: int, prev_node: 'MyListNode' = None, next_node: 'MyListNode' = None):
         self.elem = elem
         self.next_node = next_node
